1fb72cbfb3db30602712f4301071ca6715534679
Deeksha Pandey <84502516+JellyOy@users.noreply.github.com>
2023-03-19 01:32:16 +0530

    Add files via upload

---
+++ TRTB.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author chauh
+ */
+class TRTB {
+
+    static void setModel(TableModel resultSetToTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setSize(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
